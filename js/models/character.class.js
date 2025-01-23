246d610158063a6a2c612db01251c2e53137f46b
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-01-23 15:30:45 +0100

    added array IMAGES_SWIMMING and animate function to Character

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -1,7 +1,30 @@
 class Character extends MovableObject {
     height = 200;
+    IMAGES_SWIMING = [
+        'img/1.Sharkie/3.Swim/1.png',
+        'img/1.Sharkie/3.Swim/2.png',
+        'img/1.Sharkie/3.Swim/3.png',
+        'img/1.Sharkie/3.Swim/4.png',
+        'img/1.Sharkie/3.Swim/5.png',
+        'img/1.Sharkie/3.Swim/6.png'
+    ];
+    currentImage = 0;
+
     constructor(){
-        super().loadImg('img/1.Sharkie/3.Swim/1.png')
+        super().loadImg('img/1.Sharkie/3.Swim/1.png');
+        this.loadImages(this.IMAGES_SWIMING);
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_SWIMING.length;
+            let path =  this.IMAGES_SWIMING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+
+        }, 100);
     }
 
     jump(){
--- js/models/movableObject.class.js
+++ js/models/movableObject.class.js
@@ -4,11 +4,21 @@ class MovableObject {
     img;
     height = 200;
     width = 200;
+    imageCache = {};
 
     loadImg(path) {
         this.img = new Image();
         this.img.src = path;
     }
+
+    loadImages(array) {
+        array.forEach(path => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
+
     moveRight() {
         console.log('moing right');
     }
