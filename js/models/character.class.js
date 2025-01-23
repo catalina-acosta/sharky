df797a08b3e8f320617dd65d25239b94aea715b9
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-01-23 14:56:54 +0100

    added animate() function to the enemy class

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -1,5 +1,5 @@
 class Character extends MovableObject {
-
+    height = 200;
     constructor(){
         super().loadImg('img/1.Sharkie/3.Swim/1.png')
     }
--- js/models/enemy.class.js
+++ js/models/enemy.class.js
@@ -1,12 +1,19 @@
 class Enemy extends MovableObject {
-
+    height = 100;
+    width = 100;
     constructor(){
         super().loadImg('img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
         this.x = 200 + Math.random() * 500;
+        this.animate();
     }
-
 
     moveLeft() {
 
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 1;
+        }, 1000 / 60);
     }
 }
