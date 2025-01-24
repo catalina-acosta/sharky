c871963023af8a642c0f2841c96cead7ae070456
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-01-24 15:02:15 +0100

    added up and down movements for sharkie. Also the fin slap animation. still not working properly but the graphics are rendered

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -9,6 +9,31 @@ class Character extends MovableObject {
         'img/1.Sharkie/3.Swim/5.png',
         'img/1.Sharkie/3.Swim/6.png'
     ];
+    IMAGES_ATTACK_BUBBLETRAP = [
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/1.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/2.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/3.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/4.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/5.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/6.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/7.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/8.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/9.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/10.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/11.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/12.png',
+    ]
+    IMAGES_ATTACK_FINSLAP = [
+        'img/1.Sharkie/4.Attack/Fin slap/1.png',
+        'img/1.Sharkie/4.Attack/Fin slap/2.png',
+        'img/1.Sharkie/4.Attack/Fin slap/3.png',
+        'img/1.Sharkie/4.Attack/Fin slap/4.png',
+        'img/1.Sharkie/4.Attack/Fin slap/5.png',
+        'img/1.Sharkie/4.Attack/Fin slap/6.png',
+        'img/1.Sharkie/4.Attack/Fin slap/7.png',
+        'img/1.Sharkie/4.Attack/Fin slap/8.png'
+    ]
+
     world;
 
     swimmingSound = new Audio('../audio/swiming.mp3');
@@ -16,6 +41,8 @@ class Character extends MovableObject {
     constructor(){
         super().loadImg('img/1.Sharkie/3.Swim/1.png');
         this.loadImages(this.IMAGES_SWIMING);
+        this.loadImages(this.IMAGES_ATTACK_BUBBLETRAP);
+        this.loadImages(this.IMAGES_ATTACK_FINSLAP);
 
         this.animate();
     }
@@ -23,12 +50,14 @@ class Character extends MovableObject {
     animate() {
         setInterval(()=>{
             this.swimmingSound.pause();
+
             if(this.world.keyboard.RIGHT && this.x > 0 && this.x < this.world.level.levelEndX){
                 this.x += this.speed;
                 this.otherDirection = false;
                 this.swimmingSound.play();
                 console.log(this.otherDirection);
             }
+
             if(this.world.keyboard.LEFT){
                 this.x -= this.speed;
                 this.otherDirection = true;
@@ -36,6 +65,19 @@ class Character extends MovableObject {
                 console.log(this.otherDirection);
                 
             }
+
+            if(this.world.keyboard.UP && this.y > -90){
+                this.y -= this.speed;
+                this.swimmingSound.play();
+                console.log(this.otherDirection);
+            }
+
+            if(this.world.keyboard.DOWN && this.y < 320){
+                this.y += this.speed;
+                this.swimmingSound.play();
+                console.log(this.otherDirection);
+            }
+
             this.world.cameraX = -this.x + 50;
         }, 1000 / 60);
 
@@ -45,6 +87,13 @@ class Character extends MovableObject {
                 this.playAnimation(this.IMAGES_SWIMING);
             }
         }, 50);
+
+        setInterval(() => {
+            if (this.world.keyboard.SPACE) {
+                // swim animation
+                this.playAnimation(this.IMAGES_ATTACK_FINSLAP);
+            }
+        }, 1000 / 60);
     }
 
     jump(){
