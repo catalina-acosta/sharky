04aff4939fd014630ae96caf92e4b041f726a2e5
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-01-27 14:52:27 +0100

    character is working using ImageArray class for storing images. Added coin bar images array to the same class

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -1,56 +1,7 @@
 class Character extends MovableObject {
     height = 200;
     speed = 5;
-    IMAGES_SWIMING = [
-        'img/1.Sharkie/3.Swim/1.png',
-        'img/1.Sharkie/3.Swim/2.png',
-        'img/1.Sharkie/3.Swim/3.png',
-        'img/1.Sharkie/3.Swim/4.png',
-        'img/1.Sharkie/3.Swim/5.png',
-        'img/1.Sharkie/3.Swim/6.png'
-    ];
-    IMAGES_ATTACK_BUBBLETRAP = [
-        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/1.png',
-        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/2.png',
-        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/3.png',
-        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/4.png',
-        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/5.png',
-        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/6.png',
-        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/7.png',
-        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/8.png',
-    ]
-    IMAGES_ATTACK_FINSLAP = [
-        'img/1.Sharkie/4.Attack/Fin slap/1.png',
-        'img/1.Sharkie/4.Attack/Fin slap/2.png',
-        'img/1.Sharkie/4.Attack/Fin slap/3.png',
-        'img/1.Sharkie/4.Attack/Fin slap/4.png',
-        'img/1.Sharkie/4.Attack/Fin slap/5.png',
-        'img/1.Sharkie/4.Attack/Fin slap/6.png',
-        'img/1.Sharkie/4.Attack/Fin slap/7.png',
-        'img/1.Sharkie/4.Attack/Fin slap/8.png'
-    ]
-    IMAGES_DEAD = [
-        'img/1.Sharkie/6.dead/1.Poisoned/1.png',
-        'img/1.Sharkie/6.dead/1.Poisoned/2.png',
-        'img/1.Sharkie/6.dead/1.Poisoned/3.png',
-        'img/1.Sharkie/6.dead/1.Poisoned/4.png',
-        'img/1.Sharkie/6.dead/1.Poisoned/5.png',
-        'img/1.Sharkie/6.dead/1.Poisoned/6.png',
-        'img/1.Sharkie/6.dead/1.Poisoned/7.png',
-        'img/1.Sharkie/6.dead/1.Poisoned/8.png',
-        'img/1.Sharkie/6.dead/1.Poisoned/9.png',
-        'img/1.Sharkie/6.dead/1.Poisoned/10.png',
-        'img/1.Sharkie/6.dead/1.Poisoned/11.png',
-        'img/1.Sharkie/6.dead/1.Poisoned/12.png',
-    ]
-    IMAGES_HURT = [
-        'img/1.Sharkie/5.Hurt/1.Poisoned/1.png',
-        'img/1.Sharkie/5.Hurt/1.Poisoned/2.png',
-        'img/1.Sharkie/5.Hurt/1.Poisoned/3.png',
-        'img/1.Sharkie/5.Hurt/1.Poisoned/4.png',
-        'img/1.Sharkie/5.Hurt/1.Poisoned/5.png',
-    ]
-
+    IMAGES = [];
     world;
 
     swimmingSound = new Audio('../audio/swiming.mp3');
@@ -63,12 +14,12 @@ class Character extends MovableObject {
     }
 
     constructor(){
-        super().loadImg('img/1.Sharkie/3.Swim/1.png');
-        this.loadImages(this.IMAGES_SWIMING);
-        this.loadImages(this.IMAGES_ATTACK_BUBBLETRAP);
-        this.loadImages(this.IMAGES_ATTACK_FINSLAP);
-        this.loadImages(this.IMAGES_DEAD);
-        this.loadImages(this.IMAGES_HURT);
+        super().loadImg(ImageArray.CHARACTER_SWIMING[0]);
+        this.loadImages(ImageArray.CHARACTER_SWIMING);
+        this.loadImages(ImageArray.CHARACTER_ATTACK_BUBBLETRAP);
+        this.loadImages(ImageArray.CHARACTER_ATTACK_FINSLAP);
+        this.loadImages(ImageArray.CHARACTER_DEAD);
+        this.loadImages(ImageArray.CHARACTER_HURT);
 
         this.animate();
     }
@@ -104,15 +55,15 @@ class Character extends MovableObject {
         setInterval(() => {
             if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
                 // swim animation
-                this.playAnimation(this.IMAGES_SWIMING);
+                this.playAnimation(ImageArray.CHARACTER_SWIMING);
             } 
         }, 50);
 
         setInterval(() => {
             if (this.isDead()) {
-                this.playAnimation(this.IMAGES_DEAD);
+                this.playAnimation(ImageArray.CHARACTER_DEAD);
             } else if (this.isHurt()) {
-                this.playAnimation(this.IMAGES_HURT); 
+                this.playAnimation(ImageArray.CHARACTER_HURT); 
             }
             
         }, 50);
@@ -120,14 +71,14 @@ class Character extends MovableObject {
         setInterval(() => {
             if (this.world.keyboard.SPACE) {
                 // swim animation
-                this.playAnimation(this.IMAGES_ATTACK_FINSLAP);
+                this.playAnimation(ImageArray.CHARACTER_ATTACK_FINSLAP);
             }
         }, 1000 / 60);
 
         setInterval(() => {
             if (this.world.keyboard.Q) {
                 // swim animation
-                this.playAnimation(this.IMAGES_ATTACK_BUBBLETRAP);
+                this.playAnimation(ImageArray.CHARACTER_ATTACK_BUBBLETRAP);
             }
         }, 1000 / 60);
     }
--- js/models/image-array.class.js
+++ js/models/image-array.class.js
@@ -1,5 +1,4 @@
 class ImageArray {
-
     static STATUSBAR_IMAGES = [
         'img/4. Marcadores/green/Life/0_  copia 3.png', 
         'img/4. Marcadores/green/Life/20_ copia 4.png', 
@@ -17,5 +16,69 @@ class ImageArray {
         'img/4. Marcadores/green/poisoned bubbles/80_ copia 2.png',
         'img/4. Marcadores/green/poisoned bubbles/100_ copia 3.png',
     ]
+
+    static COIN_BAR = [
+        'img/4. Marcadores/Purple/0_ _1.png',
+        'img/4. Marcadores/Purple/20_ .png',
+        'img/4. Marcadores/Purple/40_ _1.png',
+        'img/4. Marcadores/Purple/60_ _1.png',
+        'img/4. Marcadores/Purple/80_ _1.png',
+        'img/4. Marcadores/Purple/100__1.png'
+    ]
+
+    static CHARACTER_SWIMING = [
+        'img/1.Sharkie/3.Swim/1.png',
+        'img/1.Sharkie/3.Swim/2.png',
+        'img/1.Sharkie/3.Swim/3.png',
+        'img/1.Sharkie/3.Swim/4.png',
+        'img/1.Sharkie/3.Swim/5.png',
+        'img/1.Sharkie/3.Swim/6.png'
+    ];
+
+    static CHARACTER_ATTACK_BUBBLETRAP = [
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/1.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/2.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/3.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/4.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/5.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/6.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/7.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/8.png',
+    ];
+
+    static CHARACTER_ATTACK_FINSLAP = [
+        'img/1.Sharkie/4.Attack/Fin slap/1.png',
+        'img/1.Sharkie/4.Attack/Fin slap/2.png',
+        'img/1.Sharkie/4.Attack/Fin slap/3.png',
+        'img/1.Sharkie/4.Attack/Fin slap/4.png',
+        'img/1.Sharkie/4.Attack/Fin slap/5.png',
+        'img/1.Sharkie/4.Attack/Fin slap/6.png',
+        'img/1.Sharkie/4.Attack/Fin slap/7.png',
+        'img/1.Sharkie/4.Attack/Fin slap/8.png'
+    ];
+
+    static CHARACTER_DEAD = [
+        'img/1.Sharkie/6.dead/1.Poisoned/1.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/2.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/3.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/4.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/5.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/6.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/7.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/8.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/9.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/10.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/11.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/12.png',
+    ];
+
+    static CHARACTER_HURT = [
+        'img/1.Sharkie/5.Hurt/1.Poisoned/1.png',
+        'img/1.Sharkie/5.Hurt/1.Poisoned/2.png',
+        'img/1.Sharkie/5.Hurt/1.Poisoned/3.png',
+        'img/1.Sharkie/5.Hurt/1.Poisoned/4.png',
+        'img/1.Sharkie/5.Hurt/1.Poisoned/5.png',
+    ];
+
 }
 
