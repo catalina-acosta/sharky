55adb5ca63dba6b7713c7800bc04497f3c3b0585
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-07 09:20:24 +0100

    refractoring and jsdoc endboss and character classes done

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -1,21 +1,71 @@
+/**
+ * Class representing the main character in the game.
+ * @extends MovableObject
+ */
 class Character extends MovableObject {
+    /**
+     * The height of the character.
+     * @type {number}
+     */
     height = 200;
-    speed = 5;
-    IMAGES = [];
-    world;
-    isAttacking = false;
-    inactivityTimer;
-    inactivityDuration = 15000;
-    isLongIdle = false; 
 
+    /**
+     * The speed of the character.
+     * @type {number}
+     */
+    speed = 5;
+
+    /**
+     * Array of image paths for the character's animations.
+     * @type {string[]}
+     */
+    IMAGES = [];
+
+    /**
+     * The world object that the character interacts with.
+     * @type {Object}
+     */
+    world;
+
+    /**
+     * Indicates whether the character is attacking.
+     * @type {boolean}
+     */
+    isAttacking = false;
+
+    /**
+     * Timer for detecting inactivity.
+     * @type {number}
+     */
+    inactivityTimer;
+
+    /**
+     * Duration of inactivity before the character goes into long idle state.
+     * @type {number}
+     */
+    inactivityDuration = 15000;
+
+    /**
+     * Indicates whether the character is in a long idle state.
+     * @type {boolean}
+     */
+    isLongIdle = false;
+
+    /**
+     * Offset values for collision detection.
+     * @type {Object}
+     */
     offset = {
         top: 90,
         bottom: 40,
         left: 40,
         right: 40
-    }
+    };
 
-    constructor(){
+    /**
+     * Creates an instance of Character.
+     */
+    constructor() {
         super().loadImg(ImageArray.CHARACTER_SWIMING[0]);
         this.loadImages(ImageArray.CHARACTER_SWIMING);
         this.loadImages(ImageArray.CHARACTER_ATTACK_BUBBLETRAP);
@@ -29,86 +79,179 @@ class Character extends MovableObject {
         this.setupInactivityListener();
     }
 
+    /**
+     * Starts the animation and movement handling for the character.
+     */
     animate() {
-        setInterval(()=>{
-            AudioLibrary.SWIMMING.pause();
-            AudioLibrary.DAMAGE.playbackRate = 0.5;
-            if(this.world.keyboard.RIGHT && this.x <= this.world.level.levelEndX){
-                this.moveRight();
-                AudioLibrary.playSound(AudioLibrary.SWIMMING);
-            } else if(this.world.keyboard.LEFT && this.x > this.world.level.levelBeginX){
-                AudioLibrary.playSound(AudioLibrary.SWIMMING);
-                this.moveLeft();
-                this.otherDirection = true;
-            }
-
-            if(this.world.keyboard.UP && this.y > -90){
-                this.moveUp();
-                AudioLibrary.playSound(AudioLibrary.SWIMMING);
-            }
-
-            if(this.world.keyboard.DOWN && this.y < 320){
-                this.moveDown();
-                AudioLibrary.playSound(AudioLibrary.SWIMMING);
-            }
-            this.world.cameraX = -this.x + 50;
-        }, 1000 / 60);
-
-        setInterval(() => {
-            if (this.isLongIdle) {
-                this.playAnimation(ImageArray.CHARACTER_LONG_IDLE);
-                AudioLibrary.playSound(AudioLibrary.SNOARING_SOUND);
-                return; 
-            }
-
-            if (this.isDead()) {
-                this.playAnimation(ImageArray.CHARACTER_DEAD);
-                AudioLibrary.playSound(AudioLibrary.GAMEOVER_SOUND);
-            } else if (this.isHurt()) {
-                this.playAnimation(ImageArray.CHARACTER_HURT); 
-                AudioLibrary.playSound(AudioLibrary.HURT_SOUND);
-            } 
-            else if (this.world.keyboard.SPACE && this.attackAnimationDone == false) {
-                AudioLibrary.playSound(AudioLibrary.FINSLAP_SOUND);
-                this.playAnimation(ImageArray.CHARACTER_ATTACK_FINSLAP, true);
-                this.isAttacking = true;
-                if(this.attackAnimationDone) {
-                    this.attackAnimationDone = false;
-                    this.world.keyboard.SPACE = false;
-                }
-            } 
-            else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT || this.world.keyboard.UP || this.world.keyboard.DOWN) {
-                this.playAnimation(ImageArray.CHARACTER_SWIMING);
-            } 
-            else if (this.world.keyboard.D && this.attackAnimationDone == false) {
-                this.playAnimation(ImageArray.CHARACTER_ATTACK_BUBBLETRAP, true);
-                AudioLibrary.playSound(AudioLibrary.BUBBLE_SOUND);
-                if(this.attackAnimationDone) {
-                    this.attackAnimationDone = false;
-                    this.world.keyboard.D = false;
-                }
-            } else {
-                this.playAnimation(ImageArray.CHARACTER_IDLE);
-            }
-        }, 100);
+        this.handleMovement();
+        this.handleAnimationStates();
     }
 
+    /**
+     * Handles the movement of the character based on keyboard input.
+     */
+    handleMovement() {
+        setInterval(() => {
+            AudioLibrary.SWIMMING.pause();
+            AudioLibrary.DAMAGE.playbackRate = 0.5;
+            this.checkKeyAndAnimation();
+            this.world.cameraX = -this.x + 50;
+        }, 1000 / 60);
+    }
+
+    /**
+     * Checks the keyboard input and updates the character's movement and animation accordingly.
+     */
+    checkKeyAndAnimation() {
+        if (this.canMoveRight()) {
+            this.moveRight();
+            AudioLibrary.playSound(AudioLibrary.SWIMMING);
+        } else if (this.canMoveLeft()) {
+            AudioLibrary.playSound(AudioLibrary.SWIMMING);
+            this.moveLeft();
+            this.otherDirection = true;
+        }
+        if (this.canMoveUp()) {
+            this.moveUp();
+            AudioLibrary.playSound(AudioLibrary.SWIMMING);
+        }
+        if (this.canMoveDown()) {
+            this.moveDown();
+            AudioLibrary.playSound(AudioLibrary.SWIMMING);
+        }
+    }
+
+    /**
+     * Checks if the character can move right.
+     * @returns {boolean} - True if the character can move right, false otherwise.
+     */
+    canMoveRight() {
+        return this.world.keyboard.RIGHT && this.x <= this.world.level.levelEndX;
+    }
+
+    /**
+     * Checks if the character can move left.
+     * @returns {boolean} - True if the character can move left, false otherwise.
+     */
+    canMoveLeft() {
+        return this.world.keyboard.LEFT && this.x > this.world.level.levelBeginX;
+    }
+
+    /**
+     * Checks if the character can move up.
+     * @returns {boolean} - True if the character can move up, false otherwise.
+     */
+    canMoveUp() {
+        return this.world.keyboard.UP && this.y > -90;
+    }
+
+    /**
+     * Checks if the character can move down.
+     * @returns {boolean} - True if the character can move down, false otherwise.
+     */
+    canMoveDown() {
+        return this.world.keyboard.DOWN && this.y < 320;
+    }
+
+    /**
+     * Handles the animation states of the character.
+     */
+    handleAnimationStates() {
+        setInterval(() => this.checkStateAndAnimation(), 100);
+    }
+
+    /**
+     * Checks the state of the character and plays the corresponding animation.
+     */
+    checkStateAndAnimation() {
+        if (this.isLongIdle) {
+            this.playSleepAnimation();
+            return;
+        } else if (this.isDead()) {
+            this.playDeadAnimation();
+        } else if (this.isHurt()) {
+            this.playHurtAnimation();
+        } else if (this.world.keyboard.SPACE && !this.attackAnimationDone) {
+            this.playFinslapAnimation();
+        } else if (this.world.keyboard.D && !this.attackAnimationDone) {
+            this.playBubbleAttack();
+        } else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT || this.world.keyboard.UP || this.world.keyboard.DOWN) {
+            this.playAnimation(ImageArray.CHARACTER_SWIMING);
+        } else {
+            this.playAnimation(ImageArray.CHARACTER_IDLE);
+        }
+    }
+
+    /**
+     * Plays the sleep animation for the character.
+     */
+    playSleepAnimation() {
+        this.playAnimation(ImageArray.CHARACTER_LONG_IDLE);
+        AudioLibrary.playSound(AudioLibrary.SNOARING_SOUND);
+    }
+
+    /**
+     * Plays the dead animation for the character.
+     */
+    playDeadAnimation() {
+        this.playAnimation(ImageArray.CHARACTER_DEAD);
+        AudioLibrary.playSound(AudioLibrary.GAMEOVER_SOUND);
+    }
+
+    /**
+     * Plays the hurt animation for the character.
+     */
+    playHurtAnimation() {
+        this.playAnimation(ImageArray.CHARACTER_HURT);
+        AudioLibrary.playSound(AudioLibrary.HURT_SOUND);
+    }
+
+    /**
+     * Plays the finslap attack animation for the character.
+     */
+    playFinslapAnimation() {
+        AudioLibrary.playSound(AudioLibrary.FINSLAP_SOUND);
+        this.playAnimation(ImageArray.CHARACTER_ATTACK_FINSLAP, true);
+        this.isAttacking = true;
+        if (this.attackAnimationDone) {
+            this.attackAnimationDone = false;
+            this.world.keyboard.SPACE = false;
+        }
+    }
+
+    /**
+     * Plays the bubble attack animation for the character.
+     */
+    playBubbleAttack() {
+        this.playAnimation(ImageArray.CHARACTER_ATTACK_BUBBLETRAP, true);
+        AudioLibrary.playSound(AudioLibrary.BUBBLE_SOUND);
+        if (this.attackAnimationDone) {
+            this.attackAnimationDone = false;
+            this.world.keyboard.D = false;
+        }
+    }
+
+    /**
+     * Sets up the inactivity listener to detect when the character is idle.
+     */
     setupInactivityListener() {
-        window.addEventListener('keydown', () => { // listens when there is a keydown
-            this.resetInactivityTimer(); // resets the timer when a key is pressed 
+        window.addEventListener('keydown', () => {
+            this.resetInactivityTimer();
         });
         window.addEventListener('touchstart', () => {
             this.resetInactivityTimer();
         });
-        this.resetInactivityTimer(); // initial reset of the timer
+        this.resetInactivityTimer();
     }
 
+    /**
+     * Resets the inactivity timer and sets the character to long idle state after a duration.
+     */
     resetInactivityTimer() {
-        clearTimeout(this.inactivityTimer); // stops the timeout
-        this.isLongIdle = false; // set to false, so that other animations can take place
+        clearTimeout(this.inactivityTimer);
+        this.isLongIdle = false;
         this.inactivityTimer = setTimeout(() => {
-            this.isLongIdle = true; 
-        }, this.inactivityDuration); // if the inactivity lasts 5 seconds, isLongIdle ist set to true
+            this.isLongIdle = true;
+        }, this.inactivityDuration);
     }
-
 }
--- js/models/endBoss.class.js
+++ js/models/endBoss.class.js
@@ -1,17 +1,59 @@
+/**
+ * Class representing the end boss in the game.
+ * @extends MovableObject
+ */
 class EndBoss extends MovableObject {
+    /**
+     * The height of the end boss.
+     * @type {number}
+     */
     height = 400;
+
+    /**
+     * The width of the end boss.
+     * @type {number}
+     */
     width = 400;
+
+    /**
+     * The y-coordinate of the end boss.
+     * @type {number}
+     */
     y = -350;
+
+    /**
+     * Offset values for collision detection.
+     * @type {Object}
+     */
     offset = {
         top: 100,
         bottom: 30,
         left: 50,
         right: 50
-    }
+    };
+
+    /**
+     * Array of image paths for the end boss's animations.
+     * @type {string[]}
+     */
     IMAGES = [];
+
+    /**
+     * The speed of the end boss.
+     * @type {number}
+     */
     speed = 8;
+
+    /**
+     * Indicates whether the end boss has had first contact with the character.
+     * @type {boolean}
+     */
     hadFirstContact = false;
 
+    /**
+     * Creates an instance of EndBoss.
+     * @param {Object} world - The world object that the end boss interacts with.
+     */
     constructor(world) {
         super();
         this.world = world;
@@ -26,44 +68,87 @@ class EndBoss extends MovableObject {
         this.i = 0;
     }
 
-    animate(){
+    /**
+     * Starts the animation handling for the end boss.
+     */
+    animate() {
         setInterval(() => {
             const distanceToCharacter = Math.abs(this.x - this.world.character.x);   
-            if(distanceToCharacter <= 500 && this.i < 8 && !this.hadFirstContact) {
-                this.y = 0;
-                this.playAnimation(ImageArray.ENDBOSS_IMAGES_INTRODUCE);
-                this.i ++;
-            } 
-            else if (this.isDead()) {
-                console.log(this.isDead());
-                this.playAnimation(ImageArray.ENDBOSS_IMAGES_DEAD);
-            } 
-            else if (this.isHurt()) {
-                AudioLibrary.playSound(AudioLibrary.DAMAGE);
-                    this.playAnimation(ImageArray.ENDBOSS_IMAGES_HURT); 
-                } 
-            
-            else if ( this.i == 8 && distanceToCharacter < 500 && distanceToCharacter > 350 ){
-                this.hadFirstContact = true;
-                this.playAnimation(ImageArray.ENDBOSS_IMAGES_FLOATING);
-            } else if (distanceToCharacter <= 350) {
-                this.playAnimation(ImageArray.ENDBOSS_IMAGES_ATTACK);
-                this.followCharacter();
-            }
+            this.handleEndBossAnimations(distanceToCharacter);
         }, 150);
     }
-    
-    followCharacter() {
-        if (this.x > this.world.character.x) { 
-            this.otherDirection = false;
-            this.x -= this.speed; 
-        } else if (this.x < this.world.character.x) { 
-            this.otherDirection = true;
-            this.x += this.speed; 
-        }
 
+    /**
+     * Handles the animations of the end boss based on the distance to the character.
+     * @param {number} distanceToCharacter - The distance to the character.
+     */
+    handleEndBossAnimations(distanceToCharacter) {
+        if (distanceToCharacter <= 500 && this.i < 8 && !this.hadFirstContact) {
+            this.playIntroAimation();
+        } else if (this.isDead()) {
+            this.playDeadAnimation();
+        } else if (this.isHurt()) {
+            this.playHurtAnimation();
+        } else if (this.i == 8 && distanceToCharacter < 500 && distanceToCharacter > 350) {
+            this.playIdleAnimation();
+        } else if (distanceToCharacter <= 350) {
+            this.playAttackAnimation();
+        }
+    }
+
+    /**
+     * Plays the introduction animation for the end boss.
+     */
+    playIntroAimation() {
+        this.y = 0;
+        this.playAnimation(ImageArray.ENDBOSS_IMAGES_INTRODUCE);
+        this.i++;
+    }
+
+    /**
+     * Plays the dead animation for the end boss.
+     */
+    playDeadAnimation() {
+        this.playAnimation(ImageArray.ENDBOSS_IMAGES_DEAD);
+    }
+
+    /**
+     * Plays the hurt animation for the end boss.
+     */
+    playHurtAnimation() {
+        AudioLibrary.playSound(AudioLibrary.DAMAGE);
+        this.playAnimation(ImageArray.ENDBOSS_IMAGES_HURT);
+    }
+
+    /**
+     * Plays the idle animation for the end boss.
+     */
+    playIdleAnimation() {
+        this.hadFirstContact = true;
+        this.playAnimation(ImageArray.ENDBOSS_IMAGES_FLOATING);
+    }
+
+    /**
+     * Plays the attack animation for the end boss and makes it follow the character.
+     */
+    playAttackAnimation() {
+        this.playAnimation(ImageArray.ENDBOSS_IMAGES_ATTACK);
+        this.followCharacter();
+    }
+
+    /**
+     * Makes the end boss follow the character.
+     */
+    followCharacter() {
+        if (this.x > this.world.character.x) {
+            this.otherDirection = false;
+            this.x -= this.speed;
+        } else if (this.x < this.world.character.x) {
+            this.otherDirection = true;
+            this.x += this.speed;
+        }
         if (this.y > this.world.character.y) {
-            this.y -= this.speed; 
+            this.y -= this.speed;
         } else if (this.y < this.world.character.y) {
             this.y += this.speed;
         }
